36a2d315b25a2f3aa3e5a449f544ded7c94cb188
Hong-En <109710572+brian52014@users.noreply.github.com>
2023-08-07 14:50:28 +0800

    Add files via upload

--- 成醫人流辨識/app2.py
+++ 成醫人流辨識/app2.py
@@ -4,28 +4,38 @@ import csv
 import cv2
 import random
 import time
-from flask import Flask, render_template, request, redirect, url_for, Response,jsonify
+from flask import Flask, render_template, request, redirect, url_for, Response,jsonify,session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 people_count = 0  # 初始化人数
 
 @app.route('/index', methods=["POST", "GET"])
 def index():
-    global start
+    global start,f
+    f=12121
     if request.method == "POST":
         date = request.form["scale"]
         locate = request.form["locate"]
-        area = request.form["area"]
+        area = float(request.form["area"])
         rtsp = request.form["rtsp"]
+        print("Received values from index form:")
         print(date)
         print(locate)
         print(area)
         print(rtsp)
         start = True
-        print(start) 
+        print(start)
 
-    return render_template('index.html')
+        # 将area存储到会话中
+
+
+        return render_template('application.html', area=area)
+    return render_template('index.html',f=f)
+
+
+
 
 def detect():
     return random.randint(0, 100)
@@ -58,9 +68,30 @@ def gen():
 
 @app.route('/application', methods=["POST", "GET"])
 def application():
-    global people_count
+    global people_count,f,area,current_time
+    
     current_time = time.strftime('%Y-%m-%d %H:%M:%S')  # 获取当前时间
-    return render_template('application.html', current_time=current_time, people_count=people_count)
+    if request.method == "POST":
+        date = request.form["scale"]
+        locate = request.form["locate"]
+        area = request.form["area"]
+        rtsp = request.form["rtsp"]
+        print("Received values from index form:")
+        print(date)
+        print(locate)
+        print(area)
+        print(rtsp)
+        start = True
+        print(start)
+        print(current_time)
+
+        # 将area存储到会话中
+
+
+        return render_template('application.html', current_time1 = current_time, people_count=people_count, area=area)
+
+    return render_template('application.html', current_time1 = current_time, people_count=people_count, area=area)
+
 
 @app.route('/video_feed')
 def video_feed():
